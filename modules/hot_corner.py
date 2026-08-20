36b95defbf8f3c49d9c7e426e6f187b1e02b6984
Henrique Claranhan <henrique.claranhan@outlook.com>
2026-08-19 21:03:43 -0400

    optimize hot corner checks and cache monitor positions

--- modules/hot_corner.py
+++ modules/hot_corner.py
@@ -2,10 +2,30 @@ import win32api
 import win32con
 import time
 from components.settings import get_feature_state, HOT_CORNER
-from miscellaneous.utils import keyup_all_keyboard_keys
+from miscellaneous.utils import keyup_all_keyboard_keys, is_app_fullscreen
 
 __state = False
-__last_state_time = None
+__last_state_time = 0
+__monitors_cache = []
+__last_monitors_update = 0
+
+
+def __get_monitors_corners():
+	global __monitors_cache, __last_monitors_update
+
+	current_time = time.time()
+	if not __monitors_cache or (current_time - __last_monitors_update) > 5.0:
+		try:
+			__monitors_cache = [
+				(rect[0], rect[1])
+				for _, _, rect in win32api.EnumDisplayMonitors()
+			]
+			__last_monitors_update = current_time
+		except Exception:
+			if not __monitors_cache:
+				__monitors_cache = [(0, 0)]
+
+	return __monitors_cache
 
 
 def __toggle_overview():
@@ -13,7 +33,7 @@ def __toggle_overview():
 
 	current_time = time.time()
 
-	if __last_state_time and current_time <= __last_state_time + 0.5:
+	if current_time <= __last_state_time + 0.5:
 		return
 
 	win_code = 0x5B
@@ -29,19 +49,26 @@ def __toggle_overview():
 	__last_state_time = current_time
 
 
-def on_move():
+def on_move(x=None, y=None):
 	global __state
 
-	x, y = win32api.GetCursorPos()
+	if not get_feature_state(HOT_CORNER):
+		__state = False
+		return True
 
-	monitor_handle = win32api.MonitorFromPoint((x, y), win32con.MONITOR_DEFAULTTONEAREST)
-	monitor_rect = win32api.GetMonitorInfo(monitor_handle)["Monitor"]
+	if x is None or y is None:
+		x, y = win32api.GetCursorPos()
 
-	if x <= monitor_rect[0] + 6 and y <= 6:
-		if not __state and get_feature_state(HOT_CORNER):
-			__toggle_overview()
-			__state = True
+	corners = __get_monitors_corners()
+	in_corner = any(left <= x <= left + 6 and top <= y <= top + 6 for left, top in corners)
+
+	if in_corner:
+		if not __state:
+			if not is_app_fullscreen(x, y):
+				__toggle_overview()
+				__state = True
 	else:
 		__state = False
 
 	return True
+
