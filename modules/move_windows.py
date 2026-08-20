e176b50cc226cbf44731a95bf3801dc684b79cd3
Henrique Claranhan <henrique.claranhan@outlook.com>
2026-08-19 21:03:55 -0400

    optimize window movement and skip single monitor setups

--- modules/move_windows.py
+++ modules/move_windows.py
@@ -8,35 +8,41 @@ from miscellaneous.virtual_desktop_accessor import VirtualDesktopAccessor
 def __get_all_windows():
 	windows = []
 
-	win32gui.EnumWindows(lambda hwnd, _: windows.append(hwnd), None)
+	def enum_callback(hwnd, _):
+		if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowTextLength(hwnd) > 0 and win32gui.GetParent(hwnd) == 0:
+			windows.append(hwnd)
 
-	visible_windows = []
-
-	for hwnd in windows:
-		if win32gui.IsWindowVisible(hwnd):
-			rect = win32gui.GetWindowRect(hwnd)
-			visible_windows.append((hwnd, rect))
-
-	return visible_windows
+	win32gui.EnumWindows(enum_callback, None)
+	return windows
 
 
 def __get_primary_monitor_info():
 	monitor = win32api.MonitorFromPoint((0, 0), win32con.MONITOR_DEFAULTTOPRIMARY)
-	monitor_info = win32api.GetMonitorInfo(monitor)
-
-	return monitor_info
+	return win32api.GetMonitorInfo(monitor)
 
 
 def move_windows_to_next_desktop(next_desktop_number):
 	if not get_feature_state(KEEP_WINDOWS):
 		return False
 
+	# If there is only one monitor, there are no secondary windows to move
+	try:
+		if len(win32api.EnumDisplayMonitors()) <= 1:
+			return False
+	except Exception:
+		pass
+
 	primary_monitor_info = __get_primary_monitor_info()
+	primary_device = primary_monitor_info.get("Device")
 	windows = __get_all_windows()
 
-	for hwnd, rect in windows:
-		window_monitor = win32api.MonitorFromWindow(hwnd, win32con.MONITOR_DEFAULTTONEAREST)
-		window_monitor_info = win32api.GetMonitorInfo(window_monitor)
+	for hwnd in windows:
+		try:
+			window_monitor = win32api.MonitorFromWindow(hwnd, win32con.MONITOR_DEFAULTTONEAREST)
+			window_monitor_info = win32api.GetMonitorInfo(window_monitor)
 
-		if window_monitor_info["Device"] != primary_monitor_info["Device"]:
-			VirtualDesktopAccessor.MoveWindowToDesktopNumber(hwnd, next_desktop_number)
+			if window_monitor_info.get("Device") != primary_device:
+				VirtualDesktopAccessor.MoveWindowToDesktopNumber(hwnd, next_desktop_number)
+		except Exception:
+			continue
+
