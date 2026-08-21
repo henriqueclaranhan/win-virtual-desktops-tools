291089499683702ee0bc1e2c28cfd9ed2c56f825
Henrique Claranhan <henrique.claranhan@outlook.com>
2026-08-21 02:33:24 -0400

    chore(release): bump version to 1.5.1

--- _version.py
+++ _version.py
@@ -1,6 +1,6 @@
 """Project version and release metadata."""
 
-__version_info__ = ("1", "5", "0")
+__version_info__ = ("1", "5", "1")
 __version__ = "v" + ".".join(__version_info__)
 __releases_url__ = "https://github.com/henriqueclaranhan/win-virtual-desktops-tools/releases"
 __releases_api_url__ = "api.github.com"
